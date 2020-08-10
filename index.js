33c61b83d28366b03ba033a1bc37a22c0ca8b2d7
Riyiike <odusanyaiyanuoluwa@gmail.com>
2020-08-09 22:39:13 -0800

    same color match

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+
+// When i click on a card callback this function
 function onCardClicked(e) {
     const target = e.currentTarget;
     console.log(target.className);
-    target.className = target.className.replace('color-hidden', '').trim();
+    
 }
+
+// keep card the same way i i havent clicked on it
+if (!clickedCard)
+    target.className = target.className.replace('color-hidden', '').trim();
+    ClickedCard = target;
+} else if(Clickedcard)
