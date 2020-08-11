2797a36ba5b2f024de44e91cfee3f94827c2529e
Riyiike <odusanyaiyanuoluwa@gmail.com>
2020-08-11 08:59:25 -0800

    setting data attribute

--- index.js
+++ index.js
@@ -2,12 +2,21 @@
 // When i click on a card callback this function
 function onCardClicked(e) {
     const target = e.currentTarget;
-    console.log(target.className);
+    // console.log(target.className);
     
-}
-
-// keep card the same way i i havent clicked on it
-if (!clickedCard)
     target.className = target.className.replace('color-hidden', '').trim();
-    ClickedCard = target;
-} else if(Clickedcard)
+    // ClickedCard = target;
+console.log(target.attr('data-color'));
+    // keep card the same way i i havent clicked on it
+    if (!clickedCard) {
+    clickedCard = target;
+    }   
+    else if (clickedCard) {
+        // if we already clicked a card, check if the new card matches the old card color
+        if (
+            clickedCard.getAttribute('data-color') === target.Attribute('data-color')
+        ) {
+            clickedCard.className += 'done'
+            target.className += 'done'
+        }
+}    }
